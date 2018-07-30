b8060c12f5ecdf2f0397c66bd646dc39d2e75f50
zhangqijie <zhangqijie@lexue.com>
2018-07-30 10:42:24 +0800

    0727

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -12,46 +12,22 @@ Page({
       content: "如在下边这个例子中，点击 inner view 会先后调用handleTap3和handleTap2(因为tap事件会冒泡到 middle view，而 middle view 阻止了 tap 事件冒泡，不再向父节点传递)，点击 middle view 会触发handleTap2，点击 outer view 会触发handleTap1。"
     }
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+  onLoad: function(options) {
+    var that = this
+    console.log(options);
+    wx.request({
+      url: 'http://localhost/weicms/index.php?s=/addon/Cms/Cms/getDetail', //仅为示例，并非真实的接口地址
+      data: { id: options.id},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        that.setData({
+          info: res.data
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
+
 })
--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -1,57 +1,47 @@
-
 //lists.js
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    newsList:[
-      { id: 1, title: "aaaa", img: "/images/weicms/1.png", cTime: "2018-07-25 14:50" },
-      { id: 2, title: "bbbb", img: "/images/weicms/2.png", cTime: "2018-07-25 14:50" },
-      { id: 3, title: "cccc", img: "/images/weicms/3.png", cTime: "2018-07-25 14:50" },
-      { id: 4, title: "dddd", img: "/images/weicms/4.png", cTime: "2018-07-25 14:50" }
-    ]
+    newsList: [],
+    lastid: 0
   },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  loadData: function (lastid){
+    var limit = 2
+    var that = this
+    wx.request({
+      url: 'http://localhost/weicms/index.php?s=/addon/Cms/Cms/getList', //仅为示例，并非真实的接口地址
+      data: {lastid: lastid,limit: limit},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        var len = res.data.length
+        that.setData({lastid: res.data[len-1].id})
+
+        var dataArr = that.data.newsList
+        var newData = dataArr.concat(res.data);
+
+        that.setData({
+          newsList: newData
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+  loadMore: function (event) {
+    var id = event.currentTarget.dataset.lastid
+    this.loadData(id);
+  },
+  onLoad: function() {
+    var that = this
+
+    // getList
+    this.loadData(0);
+    
+  },
+  toastChange: function() {
+    this.setData({toastHidden:true})
+    console.log('123');
   }
 })
