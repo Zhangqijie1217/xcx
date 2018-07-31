4207fc70f8669cb8bba22d3e79288deb828e7a90
zhangqijie <zhangqijie@lexue.com>
2018-07-31 18:47:37 +0800

    0731

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -7,18 +7,25 @@ Page({
     newsList: [],
     lastid: 0
   },
-  loadData: function (lastid){
+  
+  loadData: function (lastid) {
     var limit = 2
     var that = this
     wx.request({
       url: 'http://localhost/weicms/index.php?s=/addon/Cms/Cms/getList', //仅为示例，并非真实的接口地址
-      data: {lastid: lastid,limit: limit},
+      data: { lastid: lastid, limit: limit },
       header: {
         'content-type': 'application/json' // 默认值
       },
       success: function (res) {
+        if(!res.data){
+          thar.setData({
+            toastHidden:false
+          })
+          return false
+        }
         var len = res.data.length
-        that.setData({lastid: res.data[len-1].id})
+        that.setData({ lastid: res.data[len - 1].id })
 
         var dataArr = that.data.newsList
         var newData = dataArr.concat(res.data);
@@ -32,16 +39,30 @@ Page({
   loadMore: function (event) {
     var id = event.currentTarget.dataset.lastid
     this.loadData(id);
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 100
+    })
   },
-  onLoad: function() {
+  onLoad: function () {
     var that = this
-
     // getList
     this.loadData(0);
-    
   },
-  toastChange: function() {
-    this.setData({toastHidden:true})
-    console.log('123');
+  alertbtn: function () {
+    wx.showToast({
+      title: '没有更多文章了',
+      icon: 'success',
+      duration: 1200
+    })
+  },
+  morebtn: function () {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 1200
+    })
   }
+
 })
