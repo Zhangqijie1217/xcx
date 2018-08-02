125e4f54222a12b50b490b69d3069c401188d067
zhangqijie <zhangqijie@lexue.com>
2018-08-02 10:36:07 +0800

    0802

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -5,30 +5,38 @@ const app = getApp()
 Page({
   data: {
     newsList: [],
-    lastid: 0
+    lastid: 0, //每条数据对应的索引
+    isfirst: 1 //只让提示wifi弹窗显示第一次
   },
-  
-  loadData: function (lastid) {
+
+  loadData: function(lastid) {
     var limit = 2
     var that = this
     wx.request({
       url: 'http://localhost/weicms/index.php?s=/addon/Cms/Cms/getList', //仅为示例，并非真实的接口地址
-      data: { lastid: lastid, limit: limit },
+      data: {
+        lastid: lastid,
+        limit: limit
+      },
       header: {
         'content-type': 'application/json' // 默认值
       },
-      success: function (res) {
-        if(!res.data){
-          thar.setData({
-            toastHidden:false
+      success: function(res) {
+        if (!res.data) {
+          wx.showToast({
+            title: '没有更多了',
+            icon: 'success',
+            duration: 2000
           })
           return false
         }
         var len = res.data.length
-        that.setData({ lastid: res.data[len - 1].id })
+        that.setData({
+          lastid: res.data[len - 1].id //给lastid赋值
+        })
 
         var dataArr = that.data.newsList
-        var newData = dataArr.concat(res.data);
+        var newData = dataArr.concat(res.data); //数据合并
 
         that.setData({
           newsList: newData
@@ -36,33 +44,44 @@ Page({
       }
     })
   },
-  loadMore: function (event) {
+  loadMore: function(event) {
     var id = event.currentTarget.dataset.lastid
-    this.loadData(id);
-    wx.showToast({
-      title: '加载中',
-      icon: 'loading',
-      duration: 100
-    })
-  },
-  onLoad: function () {
-    var that = this
-    // getList
-    this.loadData(0);
-  },
-  alertbtn: function () {
-    wx.showToast({
-      title: '没有更多文章了',
-      icon: 'success',
-      duration: 1200
-    })
-  },
-  morebtn: function () {
-    wx.showToast({
-      title: '加载中',
-      icon: 'loading',
-      duration: 1200
-    })
-  }
+    var isfirst = event.currentTarget.dataset.isfirst
 
+    wx.getNetworkType({
+      success: function(res) {
+        // 返回网络类型, 有效值：
+        // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
+        var networkType = res.networkType
+        if (networkType != 'wifi' && isfirst == '1') {
+          wx.showModal({
+            title: '温馨提示',
+            content: '您当前使用的是流量',
+            showCancel: false,
+            confirmText: 'ok',
+            success: function(res) {
+
+            }
+          })
+        }
+      },
+      complete: function(res) {
+        wx.showLoading({
+          title: '加载中',
+        })
+        setTimeout(function() {
+          wx.hideLoading()
+        }, 1000)
+      }
+    })
+    this.setData({
+      isfirst: 0
+    })
+    this.loadData(id);
+  },
+  onLoad: function() {
+    var that = this
+
+    this.loadData(0);
+  }
 })
